262bcde59edfc597590d320d6cbc68d62454273b
BryceFranzen <fra12032@byui.edu>
2014-10-20 19:27:40 -0600

    file update

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tally</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        This is where text goes.
+    </body>
+</html>
