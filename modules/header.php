b2a600b588e8ecdcc0f381bb9ae417ba4ef437b6
BryceFranzen <fra12032@byui.edu>
2014-10-22 17:24:14 -0600

    Edit Menu Again

--- modules/header.php
+++ modules/header.php
@@ -7,12 +7,12 @@
         <li class="Label">Main Menu</li>
         <?php if($_SESSION['is_logged_in'] == 1){?>
             <li><a href="/countScreen"><img class="menuImg" src="/images/count.png" alt="Calendar Image"/>Counter</a></li>
-            <li><a href="/"><img class="menuImg" src="/images/calendar.png" alt="Calendar Image"/>Event Menu</a></li>
-            <li><a href="/"><img class="menuImg" src="/images/undo.png" alt="Reset Image"/>Reset Count</a></li>
-            <li><a href="/"><img class="menuImg" src="/images/interval.png" alt="Interval Image"/>Change Interval</a></li>
-            <li><a href="/"><img class="menuImg" src="/images/submit.png" alt="Submit Image"/>Submit Count</a></li>
+            <li><a href="/"><img class="menuImg" src="/images/calendar.png" alt="Calendar Image"/>Event Choice Menu</a></li>
+            <li><a href="/"><img class="menuImg" src="/images/undo.png" alt="Reset Image"/>Reset Count to Zero</a></li>
+            <li><a href="/"><img class="menuImg" src="/images/interval.png" alt="Interval Image"/>Change Count Interval</a></li>
+            <li><a href="/"><img class="menuImg" src="/images/submit.png" alt="Submit Image"/>Submit Final Count</a></li>
             <li class="Spacer"></li>
-            <li><a href="/accounts/?action=logout"><img class="menuImg" src="/images/White Lock.png" alt="Logout Image"/>Logout</a></li>
+            <li><a href="/accounts/?action=logout"><img class="menuImg" src="/images/White Lock.png" alt="Logout Image"/>Logout of Organization</a></li>
         <?php } else{ ?>
             <li><a href="/about"><img class="menuImg" src="/images/about.png" alt="about Tally"/>Learn About Tally</a></li>
             <li><a href="/"><img class="menuImg" src="/images/White Lock.png" alt="Logout Image"/>Account Login</a></li>
