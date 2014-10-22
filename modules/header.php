693e674763051b0f9655f15a7faa20817cf9c60b
BryceFranzen <fra12032@byui.edu>
2014-10-22 17:00:14 -0600

    Edit Menu
    Non-Logged-in Menu Phrasing and Style Edited

--- modules/header.php
+++ modules/header.php
@@ -15,8 +15,10 @@
             <li><a href="/accounts/?action=logout"><img class="menuImg" src="/images/White Lock.png" alt="Logout Image"/>Logout</a></li>
         <?php } else{ ?>
             <li><a href="/about"><img class="menuImg" src="/images/about.png" alt="about Tally"/>Learn About Tally</a></li>
-            <li><a href="/"><img class="menuImg" src="/images/White Lock.png" alt="Logout Image"/>Login</a></li>
-            <li><a href="../accounts/createAccount.php"><img class="menuImg" src="/images/count.png" alt="Create Account Image"/>Create Account</a></li>
+            <li><a href="/"><img class="menuImg" src="/images/White Lock.png" alt="Logout Image"/>Account Login</a></li>
+            <li class="Spacer"></li>
+            <li style="line-height: 30px" class="Label">Don't Have an Account?</li>
+            <li><a href="../accounts/createAccount.php"><img class="menuImg" src="/images/count.png" alt="Create Account Image"/>Create an Account</a></li>
         <?php } ?>    
     </ul>
 </nav>
