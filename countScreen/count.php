eaf351bfc431e39f195806e74d58d0ada38d0ca8
Catherine Roebuck <c.agnes.c@gmail.com>
2014-10-30 12:52:30 -0600

    built layout for count screen

--- countScreen/count.php
+++ countScreen/count.php
@@ -1,11 +1,10 @@
 <?php 
-if(!$_SESSION){
-   session_start();
-}
-if (!$_SESSION['is_logged_in'] == 1 ){
-     header('Location: ../index.php');
-     exit;
- }
+//if(!$_SESSION){
+ //  session_start();
+//}
+//if (!$_SESSION['is_logged_in'] == 1 ){
+//     exit;
+ //}
 
 ?>
 <!DOCTYPE html>
@@ -14,19 +13,21 @@ if (!$_SESSION['is_logged_in'] == 1 ){
         <meta charset="UTF-8">
         <title>Tally, The new way to count</title>
         <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/script.php'; ?>
-        <link rel="stylesheet" type="text/css" href="/formStyle.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        <div>
-            <div id='header'>
+        <div id='header'>
                Counter
-            </div>
-            <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/header.php' ?>
-            <main>
-                <div id="errors"><?php echo $errors?></div>
-                <h1>Counter will be here...</h1>
-            </main>
         </div>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/header.php' ?>
+            <div class="count">
+                <div id="errors"><?php echo $errors?></div>
+                <h2 class="count">Your Count</h2>
+                <div id="personalCount">3285</div>
+                <h2 class="count">Total</h2>
+                <div id="totalCount">7432</div>
+                <div id="minus">-</div>
+                <div id="plus">+</div>
+        	</div>
     </body
-    <?php unset($_SESSION['message']);?>
 </html>
